7c59940a172d7302f9ccc1787b2960b1b9a1a21c
Jeff Prestes <jeffprestes@gmail.com>
2019-08-24 12:18:12 +0200

    generating and compiling go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
 
 	"github.com/ethereum/go-ethereum/common/compiler"
 )
@@ -16,6 +19,12 @@ contract test {
    }
 }
 `
+	testGoSource = `package main
+	import "fmt"
+	func main() {
+		fmt.Println("I was compiled dinamically")
+	}
+	`
 )
 
 func main() {
@@ -23,25 +32,59 @@ func main() {
 	contracts, err := compiler.CompileSolidityString("", testSource)
 	if err != nil {
 		fmt.Printf("error compiling source. result %v: %v", contracts, err)
+		return
 	}
 	if len(contracts) != 1 {
 		fmt.Printf("one contract expected, got %d", len(contracts))
+		return
 	}
 	c, ok := contracts["test"]
 	if !ok {
 		c, ok = contracts["<stdin>:test"]
 		if !ok {
 			fmt.Println("info for contract 'test' not present in result")
+			return
 		}
 	}
 	if c.Code == "" {
 		fmt.Println("empty code")
+		return
 	}
 	if c.Info.Source != testSource {
 		fmt.Println("wrong source")
+		return
 	}
 	if c.Info.CompilerVersion == "" {
 		fmt.Println("empty version")
+		return
 	}
-	fmt.Printf("\nCompiled Contract %#v\n", contracts["<stdin>:test"])
+	fmt.Printf("\nCompiled Contract %#v\n\n", contracts["<stdin>:test"])
+
+	if err := ioutil.WriteFile("./tmp/teste.go", []byte(testGoSource), 0600); err != nil {
+		fmt.Printf("Failed to write ABI binding: %v", err)
+		return
+	}
+	var out, stderr bytes.Buffer
+	cmd := exec.Command("go", "build", "-o", "./tmp/teste", "./tmp/teste.go")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Failed to compile Go code: %v\n%v", err, stderr.String())
+		return
+	}
+	fmt.Println("Binary generated: ", out.String())
+
+	out.Reset()
+	stderr.Reset()
+
+	cmd = exec.Command("./tmp/teste")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Failed to execute Go code dinamically generated: %v\n%v", err, stderr.String())
+		return
+	}
+	fmt.Println("Binary executed: ", out.String())
 }
