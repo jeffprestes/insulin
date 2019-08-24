40f590905e058f01547879c7df3c6716c8d551b3
Jeff Prestes <jeffprestes@gmail.com>
2019-08-24 11:04:27 +0200

    first compilation

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common/compiler"
+)
+
+const (
+	testSource = `
+pragma solidity >0.0.0;
+contract test {
+   /// @notice Will multiply ` + "`a`" + ` by 7.
+   function multiply(uint a) public returns(uint d) {
+       return a * 7;
+   }
+}
+`
+)
 
 func main() {
 	fmt.Println("O Rio de Janeiro continua lindo...")
+	contracts, err := compiler.CompileSolidityString("", testSource)
+	if err != nil {
+		fmt.Printf("error compiling source. result %v: %v", contracts, err)
+	}
+	if len(contracts) != 1 {
+		fmt.Printf("one contract expected, got %d", len(contracts))
+	}
+	c, ok := contracts["test"]
+	if !ok {
+		c, ok = contracts["<stdin>:test"]
+		if !ok {
+			fmt.Println("info for contract 'test' not present in result")
+		}
+	}
+	if c.Code == "" {
+		fmt.Println("empty code")
+	}
+	if c.Info.Source != testSource {
+		fmt.Println("wrong source")
+	}
+	if c.Info.CompilerVersion == "" {
+		fmt.Println("empty version")
+	}
+	fmt.Printf("\nCompiled Contract %#v\n", contracts["<stdin>:test"])
 }
