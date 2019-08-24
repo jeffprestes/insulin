401844f84592c68c36600a1ae2e4b87709c2220c
Jeff Prestes <jeffprestes@gmail.com>
2019-08-24 18:17:43 +0200

    creating and executing tests

--- main.go
+++ main.go
@@ -1,94 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"os/exec"
-	"regexp"
-	"strings"
-	"text/template"
-
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common/compiler"
-	"github.com/ethereum/go-ethereum/crypto"
 
 	"gopkg.in/urfave/cli.v1"
 )
-
-func compile(testSource string, fileInfo os.FileInfo) {
-	fmt.Println("Compiling contracts...")
-	contracts, err := compiler.CompileSolidityString("", testSource)
-	if err != nil {
-		fmt.Printf("error compiling source. result %v: %v", contracts, err)
-		return
-	}
-
-	var (
-		abis  []string
-		bins  []string
-		types []string
-		sigs  []map[string]string
-		libs  = make(map[string]string)
-	)
-
-	for name, contract := range contracts {
-		c, ok := contracts[name]
-		if !ok {
-			c, ok = contracts["<stdin>:"+name]
-			if !ok {
-				fmt.Println("\nCompilation failed :(")
-				return
-			}
-		}
-		fmt.Printf("\nCompiled Contract\n")
-
-		// writing to d
-		file, err := json.MarshalIndent(c, "", " ")
-		if err != nil {
-			fmt.Printf("Failed to parse ABIs from compiler output: %v", err)
-		}
-
-		os.Mkdir("artifacts", 0700)
-		err = ioutil.WriteFile("./artifacts/"+strings.Split(fileInfo.Name(), ".")[0]+".json", file, 0644)
-		if err != nil {
-			fmt.Printf("Failed to write file to disk: %v", err)
-		}
-
-		abi, err := json.Marshal(contract.Info.AbiDefinition) // Flatten the compiler parse
-		if err != nil {
-			fmt.Printf("Failed to parse ABIs from compiler output: %v", err)
-		}
-		abis = append(abis, string(abi))
-		bins = append(bins, contract.Code)
-		sigs = append(sigs, contract.Hashes)
-		nameParts := strings.Split(name, ":")
-		types = append(types, nameParts[len(nameParts)-1])
-
-		libPattern := crypto.Keccak256Hash([]byte(name)).String()[2:36]
-		libs[libPattern] = nameParts[len(nameParts)-1]
-	}
-
-	//fmt.Println("abis: ", abis, "bins: ", bins, "sigs: ", sigs, "types: ", types, "libs: ", libs)
-
-	code, err := bind.Bind(types, abis, bins, sigs, "proxycontracts", bind.LangGo, libs)
-	if err != nil {
-		fmt.Println("Failed to generate ABI binding:", err)
-		return
-	}
-
-	if err := os.MkdirAll("tmp/proxycontracts", 0700); err != nil {
-		fmt.Println("Failed to create directory:", err)
-		return
-	}
-
-	if err := ioutil.WriteFile("./tmp/proxycontracts/"+strings.Split(fileInfo.Name(), ".")[0]+".go", []byte(code), 0600); err != nil {
-		fmt.Println("Failed to write ABI binding", err)
-	}
-}
 
 const (
 	commandHelperTemplate = `{{.Name}}{{if .Subcommands}} command{{end}}{{if .Flags}} [command options]{{end}} [arguments...]
@@ -121,88 +38,9 @@ func init() {
 	cli.CommandHelpTemplate = commandHelperTemplate
 }
 
-func callComp(c *cli.Context) (err error) {
-	solidityPath := c.Args().Get(0)
-	files, err := ioutil.ReadDir(solidityPath)
-	if err != nil {
-		fmt.Println("callComp error: ", err)
-		return
-	}
-
-	for _, file := range files {
-
-		r, err := regexp.MatchString(".sol", file.Name())
-		if r {
-			data, err := ioutil.ReadFile(file.Name())
-			if err != nil {
-				fmt.Println("Error reading extension", err)
-			}
-			compile(string(data), file)
-			createTestFile(file)
-			runTestFile(file)
-		}
-		if err != nil {
-			fmt.Println("File reading error", err)
-			return err
-		}
-
-	}
-	return
-}
-
 func main() {
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func createTestFile(fileInfo os.FileInfo) (err error) {
-	tmp := strings.Split(fileInfo.Name(), ".")
-	fileName := tmp[0]
-	templateFile, err := template.ParseFiles("./testsbaseline.tmpl")
-	if err != nil {
-		fmt.Printf("Failed to open template file: %v", err)
-		return
-	}
-	file, err := os.Create("./tmp/" + fileName + ".go")
-	if err != nil {
-		fmt.Printf("Failed to create test file: %v", err)
-		return
-	}
-	err = templateFile.Execute(file, "")
-	if err != nil {
-		log.Print("execute: ", err)
-		return
-	}
-	file.Close()
-	var out, stderr bytes.Buffer
-	cmd := exec.Command("go", "build", "-o", "./tmp/"+fileName, "./tmp/"+fileName+".go")
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-	if err != nil {
-		fmt.Printf("Failed to compile Go code: %v\n%v", err, stderr.String())
-		return
-	}
-	fmt.Println("Binary generated: ", out.String())
-	return
-}
-
-func runTestFile(fileInfo os.FileInfo) (err error) {
-	tmp := strings.Split(fileInfo.Name(), ".")
-	fileName := tmp[0]
-
-	var out, stderr bytes.Buffer
-
-	cmd := exec.Command("./tmp/" + fileName)
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-	if err != nil {
-		fmt.Printf("Failed to execute Go code dinamically generated: %v\n%v", err, stderr.String())
-		return
-	}
-	fmt.Println("Binary executed: ", out.String())
-	return
 }
---
+++ testsjsmochabaseline.tmpl
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/jeffprestes/goethereumhelper"
+	"github.com/jeffprestes/insulin/tmp/proxycontracts"
+
+	. "github.com/franela/goblin"
+)
+
+var auth *bind.TransactOpts
+var backend *backends.SimulatedBackend
+var contractAddress common.Address
+var trx *types.Transaction
+var contractInstance *proxycontracts.Registro
+
+func Test(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("Registro must be deployed and return the message", func() {
+		// Passing Test
+		g.It("Should be deployed ", func() {
+			var err error
+			auth, backend, _ = goethereumhelper.GetMockBlockchain()
+			contractAddress, trx, contractInstance, err = proxycontracts.DeployRegistro(auth, backend)
+			if err != nil {
+				g.Fail(fmt.Sprintf("error when trying to compile contract: %v\n", err))
+			}
+			backend.Commit()
+			g.Assert(err).Equal(nil)
+
+		})
+		// Failing Test
+		g.It("Should bring message", func() {
+			var err error
+			msg, err := contractInstance.Mensagem(nil)
+			if err != nil {
+				g.Fail(fmt.Sprintf("It was not possible to bring the message: %v\n", err))
+			}
+			g.Assert(msg).Equal("Uma boa e pacifica morte para todos...")
+			fmt.Println("Contract Address: ", contractAddress.String(), " - Transaction: ", trx.Hash().String(), " - Message stored: ", msg)
+		})
+	})
+}
