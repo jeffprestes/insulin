84bc4e6310e4aeb55b9997e90729f81a35469878
fabiohild <fabiohildebrand@gmail.com>
2019-08-24 09:53:42 -0300

    merging morning work

--- main.go
+++ main.go
@@ -10,46 +10,15 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common/compiler"
 	"github.com/ethereum/go-ethereum/crypto"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
-const (
-	testSource = `
-pragma solidity >0.0.0;
-contract testaaa {
-   /// @notice Will multiply ` + "`a`" + ` by 7.
-   function multiply(uint a) public returns(uint d) {
-       return a * 7;
-   }
-}
-`
-)
-
-func main() {
+func compile(testSource string) {
 	fmt.Println("Compiling contracts...")
 	contracts, err := compiler.CompileSolidityString("", testSource)
 	if err != nil {
 		fmt.Printf("error compiling source. result %v: %v", contracts, err)
-	}
-	c, ok := contracts["testaaa"]
-	if !ok {
-		c, ok = contracts["<stdin>:testaaa"]
-		if !ok {
-			fmt.Println("\nCompilation failed :(")
-			return
-		}
-	}
-	fmt.Printf("\nCompiled Contract\n")
-
-	// writing to d
-	file, err := json.MarshalIndent(c, "", " ")
-	if err != nil {
-		fmt.Printf("Failed to parse ABIs from compiler output: %v", err)
-	}
-
-	os.Mkdir("artifacts", 0700)
-	err = ioutil.WriteFile("./artifacts/testaaa.json", file, 0644)
-	if err != nil {
-		fmt.Printf("Failed to write file to disk: %v", err)
 	}
 
 	var (
@@ -60,8 +29,29 @@ func main() {
 		libs  = make(map[string]string)
 	)
 
-	// Gather all non-excluded contract for binding
 	for name, contract := range contracts {
+		c, ok := contracts[name]
+		if !ok {
+			c, ok = contracts["<stdin>:"+name]
+			if !ok {
+				fmt.Println("\nCompilation failed :(")
+				return
+			}
+		}
+		fmt.Printf("\nCompiled Contract\n")
+
+		// writing to d
+		file, err := json.MarshalIndent(c, "", " ")
+		if err != nil {
+			fmt.Printf("Failed to parse ABIs from compiler output: %v", err)
+		}
+
+		os.Mkdir("artifacts", 0700)
+		err = ioutil.WriteFile("./artifacts/testaaa.json", file, 0644)
+		if err != nil {
+			fmt.Printf("Failed to write file to disk: %v", err)
+		}
+
 		abi, err := json.Marshal(contract.Info.AbiDefinition) // Flatten the compiler parse
 		if err != nil {
 			fmt.Printf("Failed to parse ABIs from compiler output: %v", err)
@@ -86,5 +76,53 @@ func main() {
 	os.MkdirAll("tmp/proxycontracts", 0700)
 	if err := ioutil.WriteFile("./tmp/proxycontracts/testaaa.go", []byte(code), 0600); err != nil {
 		fmt.Println("Failed to write ABI binding: %v", err)
+	}
+}
+
+const (
+	commandHelperTemplate = `{{.Name}}{{if .Subcommands}} command{{end}}{{if .Flags}} [command options]{{end}} [arguments...]
+{{if .Description}}{{.Description}}
+{{end}}{{if .Subcommands}}
+SUBCOMMANDS:
+	{{range .Subcommands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t" }}{{.Usage}}
+	{{end}}{{end}}{{if .Flags}}
+OPTIONS:
+{{range $.Flags}}{{"\t"}}{{.}}
+{{end}}
+{{end}}`
+)
+
+var (
+	app *cli.App
+)
+
+func init() {
+	app = cli.NewApp()
+	app.Name = "Insulin"
+	app.Commands = []cli.Command{
+		{
+			Name:    "compile",
+			Aliases: []string{"c"},
+			Usage:   "compile Solidity contract",
+			Action:  callComp,
+		},
+	}
+	cli.CommandHelpTemplate = commandHelperTemplate
+}
+
+func callComp(c *cli.Context) error {
+	SolidityPath := c.Args().Get(0)
+	data, err := ioutil.ReadFile(SolidityPath)
+	if err != nil {
+		fmt.Println("File reading error", err)
+	}
+	compile(string(data))
+	return nil
+}
+
+func main() {
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
---
+++ registro.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+contract Registro {
+    string public mensagem;
+
+    constructor () public {
+        mensagem = "Uma boa e pacifica morte para todos...";
+    }
+
+    function RegistrarMensagem(string memory _mensagem) public {
+        mensagem = _mensagem;
+        emit NovaMensagem(mensagem);
+    }
+
+    event NovaMensagem(
+        string _novaMsg
+    );
+}
